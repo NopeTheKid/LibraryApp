38fffefbe05ea475f92efd19fc6178ab76855622
edgarcBrito <129171006+edgarcBrito@users.noreply.github.com>
2023-07-01 05:05:33 +0100

    Activity login e fragment inicial

---
+++ app/src/main/java/com/nopearino/libraryapp/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.nopearino.libraryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

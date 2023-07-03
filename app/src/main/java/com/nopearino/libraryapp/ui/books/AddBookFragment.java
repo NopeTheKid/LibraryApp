66bf0f792b6bb651c12206d5138e4335cb5fb6fb
Nopearino <ajmc.p@hotmail.com>
2023-07-03 03:50:21 +0100

    Add book

--- app/src/main/java/com/nopearino/libraryapp/ui/books/AddBookFragment.java
+++ app/src/main/java/com/nopearino/libraryapp/ui/books/AddBookFragment.java
@@ -1,69 +1,45 @@
 package com.nopearino.libraryapp.ui.books;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
 
-import com.nopearino.libraryapp.R;
+import com.nopearino.libraryapp.databinding.FragmentAddBookBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddBookFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddBookFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
-    public AddBookFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_AddBook.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddBookFragment newInstance(String param1, String param2) {
-        AddBookFragment fragment = new AddBookFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    FragmentAddBookBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_book, container, false);
+        binding = FragmentAddBookBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.spinner.setSelection(0);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.ETextAutor.setText("");
+                binding.ETextISBN.setText("");
+                binding.ETextTitulo.setText("");
+                binding.spinner.setSelection(0);
+                Toast toast = Toast.makeText(getContext(), "Livro adicionado", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        return root;
 
     }
 }

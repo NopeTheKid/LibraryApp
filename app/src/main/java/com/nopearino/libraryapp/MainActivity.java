9fab4d22e883a29eea3cbb119233bc3a3e9f6b91
Nopearino <ajmc.p@hotmail.com>
2023-07-03 04:00:21 +0100

    Colors

--- app/src/main/java/com/nopearino/libraryapp/MainActivity.java
+++ app/src/main/java/com/nopearino/libraryapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nopearino.libraryapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setBackgroundColor(Color.rgb(46, 70, 92));
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_add_book, R.id.navigation_notifications)
                 .build();
